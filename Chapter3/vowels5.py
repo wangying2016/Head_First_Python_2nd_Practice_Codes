3c60c28969008cdc5caa2ae082b919216cc8649c
Wang Ying <wy867504146@outlook.com>
2017-09-05 16:31:14 +0800

    Chapter3 Codes

--- Chapter3/vowels5.py
+++ Chapter3/vowels5.py
@@ -1,10 +1,12 @@
 
 vowels = ['a', 'e', 'i', 'o', 'u']
 word = input("Provide a word to search for vowels:")
-found = []
+
+found = {}
+
 for letter in word:
     if letter in vowels:
-        if letter not in found:
-            found.append(letter)
-for vowel in found:
-    print(vowel)
+            found[letter] += 1
+            
+for k, v in sorted(found.items()):
+    print(k, 'was found', v, 'time(s).')
