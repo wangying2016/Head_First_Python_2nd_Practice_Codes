7fd393d705f9a10e55c391401235b3bff405fe11
Wang Ying <wy867504146@outlook.com>
2017-09-09 11:26:54 +0800

    Chapter4 Codes

--- Chatper4/vsearch.py
+++
@@ -1,7 +0,0 @@
-
-def search4vowels(word):
-    """Display any vowels found in a supplied word."""
-    vowels = set('aeiou')
-    found = vowels.intersection(set(word))
-    for vowel in found:
-        print(vowel)
