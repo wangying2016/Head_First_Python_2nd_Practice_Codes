c2bf2f983c511be5a58fad07f88ba85acf2e4752
Wang Ying <wy867504146@outlook.com>
2017-09-23 14:48:12 +0800

    Chapter 6

--- Chapter6/vsearch4web.py
+++ Chapter6/vsearch4web.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, escape
 from vsearch import search4letters
 
 app = Flask(__name__)
@@ -7,7 +7,12 @@ app = Flask(__name__)
 
 def log_request(req: 'flask_request', res: str) -> None:
     with open('vsearch.log', 'a') as log:
-        print(req, res, file=log)
+        print(req.form,
+              req.remote_addr,
+              req.user_agent,
+              res,
+              file=log,
+              sep='|')
 
 
 @app.route('/search4', methods=['POST'])
@@ -29,6 +34,21 @@ def do_search() -> 'html':
 def entry_page() -> 'html':
     return render_template('entry.html',
                            the_title='Welcome to search4letters on the Web!')
+
+
+@app.route('/viewlog')
+def view_the_log() -> 'html':
+    contents = []
+    with open('vsearch.log') as log:
+        for line in log:
+            contents.append([])
+            for item in line.split('|'):
+                contents[-1].append(escape(item))
+    titles = ('From data', 'Remote_addr', 'User_agent', 'Results')
+    return render_template('viewlog.html',
+                           the_title='View log',
+                           the_row_titles=titles,
+                           the_data=contents,)
 
 
 if __name__ == '__main__':
